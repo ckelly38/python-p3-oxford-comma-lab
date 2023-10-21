4df560ecea5ef94d0b908aedd301d05dbb641f90
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-10-20 21:54:11 -0600

    this inserts the oxford comma between items in a list of strings

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,15 @@
 def oxford_comma(items):
-    return None
+    if (items == None): return None;
+    elif (len(items) < 1): return "";
+    elif (len(items) == 1): return items[0];
+    else: pass;
+    myjnitems = [];
+    for i in range(len(items) - 1):
+        myjnitems.append(items[i]);
+    mycmaspcstr = ", ";
+    myjnstr = mycmaspcstr.join(myjnitems);
+    #print(f"myjnstr = {myjnstr}");
+    if (len(items) == 2): return myjnstr + " and " + items[len(items) - 1];
+    else: return myjnstr + mycmaspcstr + "and " + items[len(items) - 1];
+
+#oxford_comma(["Tron", "Alan", "Clu", "Kevin Flynn"]);
